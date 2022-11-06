8693509f831764f4dc4005da8298a8fe078945c0
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-11-07 00:10:37 +0400

    Initial Commit

---
+++ HW_2.06_SergeySokolov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.06_SergeySokolov
+//
+//  Created by MacBook Air 13 on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
