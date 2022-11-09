e7e7456096086f50ec50cc3f7d63bbfe19cf385b
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-11-10 00:45:18 +0400

    5

--- HW_2.06_SergeySokolov/ViewControllers/WelcomeViewController.swift
+++ HW_2.06_SergeySokolov/ViewControllers/WelcomeViewController.swift
@@ -10,24 +10,19 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var personalInfo: UILabel!
     
     var user = ""
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeLabel.text = "Hello \(user)!"
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = "Hello \(user.login)!"
+        personalInfo.text = "Name: "
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
